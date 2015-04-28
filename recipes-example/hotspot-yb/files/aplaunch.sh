5740591e5099560daa18e2d275ddde23def65a6b
Irfan Sadiq <irfan_sadiq@mentor.com>
2015-04-28 16:47:39 +0500

    AP Launch: Script modified
    Added full paths to avoid failures.
    Added rfkill unblock statement.
    wlan0 up added.
    
    Signed-off-by: Irfan Sadiq <hmirfansadiq@gmail.com>

--- recipes-example/hotspot-yb/files/aplaunch.sh
+++ recipes-example/hotspot-yb/files/aplaunch.sh
@@ -3,9 +3,11 @@
 set -x
 echo Wait a few seconds for the USB to be ready
 sleep 20
+rfkill unblock wifi
 echo Initialising wireless interface
-ifconfig wlan0 inet 192.168.11.65
+ifconfig wlan0 inet 192.168.11.65 up
 echo Starting DHCP server
 /usr/sbin/udhcpd -f -S /etc/udhcpd.conf.yb &
 echo Launching Access Point
 /usr/sbin/hostapd /etc/hostapd.conf.yb
+/lib/systemd/scripts/setup-gw.sh
